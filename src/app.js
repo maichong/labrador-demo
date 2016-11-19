ed61132e4c7a22c3230ae038bd4a7df5f33730cc
李宇登 <li@maichong.it>
2016-11-19 17:22:07 +0800

    init

---
+++ src/app.js
@@ -0,0 +1,11 @@
+/**
+ * @copyright Maichong Software Ltd. 2016 http://maichong.it
+ * @date 2016-11-19
+ * @author Li <li@maichong.it>
+ */
+
+export default class {
+  onLaunch(){
+    
+  }
+}
---
+++ src/pages/index/index.js
@@ -0,0 +1,16 @@
+/**
+ * @copyright Maichong Software Ltd. 2016 http://maichong.it
+ * @date 2016-11-19
+ * @author Li <li@maichong.it>
+ */
+import { Component, PropTypes } from 'labrador-immutable';
+
+export default class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  onUpdate(nextProps){
+    
+  }
+}
